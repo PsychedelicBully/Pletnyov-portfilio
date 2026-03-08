d49a515a54421c6634a3cb379d28d5b86bc18fef
PsychedelicBully <pletnyovstepan@gmail.com>
2026-03-08 09:55:08 +0500

    new icon

--- public/script.js
+++ public/script.js
@@ -100,7 +100,7 @@ class PortfolioGallery {
         const loader = document.createElement('div');
         loader.id = 'gallery-loader';
         loader.style.cssText = 'padding: 40px; color: var(--text-secondary);';
-        loader.innerHTML = '<img src="/icon.png" alt="">';
+        loader.innerHTML = '<img src="/icon_loader" alt="">';
         this.galleryEl.appendChild(loader);
 
         // Fetch all posts silently
